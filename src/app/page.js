6818a5155dbdbb892f795c9757133850e39ed9b3
25LP5143 25LP5143 <25LP5143@G305-CC0240.local>
2025-09-16 12:38:18 +0800

    fixed

--- src/app/page.js
+++ src/app/page.js
@@ -2,23 +2,61 @@
 import Image from "next/image";
 import "./index.css";
 import { useState } from "react";
+import { errorToJSON } from "next/dist/server/render";
+
+const checkIfInputHasCharacters = (string) => {
+  return /\d/.test(string);
+};
+const checkIfInputHasNumbers = (string) => {
+  return /\d/.test(string);
+};
 
 export default function Home() {
-  const [firstName, setfirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [userName, setUserName] = useState("");
-  const [firstNameError, setFirstNameEroor] = useState(false);
+  const firstName = "";
+  const lastName = "";
+  const userName = "";
 
-  const handleChangeFirstNameInput = (e) => {
-    setfirstName(e.target.value);
+  const [formValues, setFormValues] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+  });
+  const [errorState, setErrorstate] = useState({});
+  const handleInputChange = (e) => {
+    const inputName = e.target.name;
+    const inputValue = e.target.value;
+    setFormValues({ ...formValues, [inputName]: inputValue });
   };
+  // const [firstName, setfirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [userName, setUserName] = useState("");
+  // const [firstNameError, setFirstNameError] = useState(false);
+  // const [lastNameError, setLastNameError] = useState(false);
+  // const [userNameError, setUserNameError] = useState(false);
+  // const containsNumberRegex = ;
+
+  // const handleChangeFirstNameInput = (e) => {
+  //   setfirstName(e.target.value);
+  // };
+  const validateInput = () => {
+    const errors = {};
+    if (checkIfInputHasNumbers(formValues.firstName)) {
+      errors.firstName =
+        "First name cannot contain special characters or numbers.";
+    }
+    return errors;
+  };
+
   const handleClickButton = () => {
-    if (firstName.length < 3) {
-      console.log(false);
+    const errors = validateInput();
+
+    if (Object.keys(errors).length === 0) {
+      setErrorstate({});
     } else {
-      console.log("true");
+      setErrorstate(errors);
     }
   };
+
   return (
     <div className="container">
       <div className="container1">
@@ -32,12 +70,24 @@ export default function Home() {
         <p>First name *</p>
         <input
           placeholder="First name"
-          onChange={handleChangeFirstNameInput}
+          onChange={handleInputChange}
+          name="firstName"
+          value={formValues.firstName}
         ></input>
+        {errorstat.firstName && (
+          <div style={{ color: "red" }}>
+            First name cannot contain special characters or numbers.
+          </div>
+        )}
       </div>
       <div>
         <p>Last name *</p>
-        <input placeholder="Last name"></input>
+        <input
+          placeholder="Last name"
+          onChange={handleInputChange}
+          name="lastName"
+          value={formValues.lastName}
+        ></input>
       </div>
       <div>
         <p>Username *</p>
